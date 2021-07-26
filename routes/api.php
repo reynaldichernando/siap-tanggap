dd4af2c76db24778404208ffbd24dedf562eb884
Reynaldi Chernando <reynaldichernando@gmail.com>
2021-07-26 10:51:32 +0700

    Add get posts endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => '/v1'], function() {
+    Route::get('/posts', [PostController::class, 'index']);
 });
