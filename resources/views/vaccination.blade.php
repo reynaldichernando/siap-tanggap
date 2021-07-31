9ea5528a8dc5385229dd87fb9513ef9765a41f6c
Reynaldi Chernando <reynaldichernando@gmail.com>
2021-07-31 16:14:50 +0700

    Add search vaccination functionality

--- resources/views/vaccination.blade.php
+++ resources/views/vaccination.blade.php
@@ -3,41 +3,52 @@
 @section('content')
 <main class="flex flex-col items-center">
     <section class="flex justify-center bg-purple-100 w-full">
-        <h1 class="text-4xl m-8">Lokasi Vaksinasi</h1>
+        <h1 class="text-4xl mt-16 mb-8">Lokasi Vaksinasi</h1>
     </section>
-    <section class="flex flex-col items-center justify-center w-full py-40 px-8 bg-purple-100">
-        <h3 class="text-xl mb-6 text-center">Cari Fasilitas Kesehatan Penyedia Vaksinasi Covid-19</h3>
-        <div
-            class="w-full md:w-4/5 lg:w-2/5 flex items-center bg-white rounded-lg px-3 py-4 justify-between form-input mb-14">
-            <input type="text" id="search-vaccination" class="w-full focus:outline-none ml-2" placeholder="Surabaya">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 cursor-pointer text-gray-600" fill="none"
-                viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
+    <section class="flex flex-col items-center justify-center w-full pt-20 pb-40 px-10 bg-purple-100">
+        <h3 class="text-xl mb-10 text-center">Cari Fasilitas Kesehatan Penyedia Vaksinasi Covid-19</h3>
+        <form action="#" method="POST" class="flex flex-col items-center w-full mb-16">
+            <div class="w-full md:w-4/5 lg:w-3/5 xl:w-2/5 flex items-center mb-4 relative">
+                <input type="text" id="search" name="search" class="w-full focus:outline-none form-input p-4 pr-14"
+                    placeholder="Klinik/Puskesmas/Kecamatan">
+                <label class=" absolute right-4">
+                    <input type="submit" class="hidden" />
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6 mr-2 cursor-pointer text-gray-400 hover:text-gray-600" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </label>
+            </div>
+
+            <div class="flex flex-col md:flex-row justify-between items-center w-full md:w-4/5 lg:w-3/5 xl:w-2/5">
+                <select name="provinceId" id="province"
+                    class="w-full lg:w-1/3 form-select rounded focus:outline-none cursor-pointer">
+                    <option value="" disabled selected>Provinsi</option>
+                </select>
+
+                <select name="cityId" id="city"
+                    class="w-full lg:w-1/3 m-2 form-select rounded focus:outline-none cursor-pointer">
+                    <option value="" disabled selected>Kota/Kabupaten</option>
+                </select>
+
+                <select name="districtId" id="district"
+                    class="w-full lg:w-1/3 form-select rounded focus:outline-none cursor-pointer">
+                    <option value="" disabled selected>Kecamatan</option>
+                </select>
+            </div>
+        </form>
+
+        <div id="hospital-container"
+            class="hidden flex-col w-full md:w-4/5 lg:w-3/5 xl:w-2/5 p-4 bg-white rounded-lg h-96 overflow-y-auto">
+            <h4 class="text-lg mb-5 text-center">List Fasilitas Kesehatan</h4>
+            <div id="hospital-list" class="divide-y-2">
+
+            </div>
         </div>
-        {{-- <div class="flex flex-col md:flex-row justify-between items-center w-5/6">
-            <select name="vacc-province" id="vacc-province"
-                class="flex-1 mx-2 border-2 border-purple-300 rounded focus:outline-none text-lg p-2 cursor-pointer">
-                <option value="">Pilih Provinsi</option>
-                <option value="1">DKI Jakarta</option>
-                <option value="2">Kepulauan Riau</option>
-            </select>
 
-            <select name="vacc-city" id="vacc-city"
-                class="flex-1 mx-2 border-2 border-purple-300 rounded focus:outline-none text-lg p-2 cursor-pointer">
-                <option value="">Pilih Kota</option>
-                <option value="1">Jakarta Barat</option>
-                <option value="2">Batam</option>
-            </select>
 
-            <select name="vacc-hospital" id="vacc-hospital"
-                class="flex-1 mx-2 border-2 border-purple-300 rounded focus:outline-none text-lg p-2 cursor-pointer">
-                <option value="">Pilih Rumah Sakit</option>
-                <option value="1">Awal Bros</option>
-                <option value="2">Klinik Tong Fang</option>
-            </select>
-        </div> --}}
     </section>
 
     <section class="flex flex-col-reverse justify-around py-16 md:flex-row items-center">
@@ -90,7 +101,8 @@
                 </p>
                 <br>
                 <p>
-                    Selain itu, dengan mengikuti vaksinasi kamu juga telah membantu mempercepat penanganan Covid-19 di masyarakat dengan pembentukan herd immunity.
+                    Selain itu, dengan mengikuti vaksinasi kamu juga telah membantu mempercepat penanganan Covid-19 di
+                    masyarakat dengan pembentukan herd immunity.
                 </p>
             </div>
         </div>
@@ -103,8 +115,7 @@
             <div
                 class="bg-white flex flex-col items-center rounded-md border-green-300 border-2 mx-5 mb-4 w-72 md:w-96">
                 <div class="">
-                    <img src="{{URL::asset('/images/astrazeneca.jpg')}}"
-                        alt="">
+                    <img src="{{URL::asset('/images/astrazeneca.jpg')}}" alt="">
                 </div>
                 <div class="p-8">
                     <h5 class="text-green-600 text-lg mb-8">AstraZeneca</h5>
@@ -120,8 +131,7 @@
 
             <div class="bg-white flex flex-col items-center rounded-md border-red-300 border-2 mx-5 mb-4 w-72 md:w-96">
                 <div class="">
-                    <img src="{{URL::asset('/images/sinovac.jpg')}}"
-                        alt="">
+                    <img src="{{URL::asset('/images/sinovac.jpg')}}" alt="">
                 </div>
                 <div class="p-8">
                     <h5 class="text-red-600 text-lg mb-8">Sinovac</h5>
@@ -138,8 +148,7 @@
             <div
                 class="bg-white flex flex-col items-center rounded-md border-purple-300 border-2 mx-5 mb-4 w-72 md:w-96">
                 <div class="">
-                    <img src="{{URL::asset('/images/sinopharm.jpg')}}"
-                        alt="">
+                    <img src="{{URL::asset('/images/sinopharm.jpg')}}" alt="">
                 </div>
                 <div class="p-8">
                     <h5 class="text-purple-600 text-lg mb-8">Sinopharm</h5>
@@ -155,3 +164,107 @@
     </section>
 </main>
 @endsection
+
+@push('scripts')
+<script>
+    let provinceSelectElement = document.querySelector('#province');
+    let citySelectElement = document.querySelector('#city');
+    let districtSelectElement = document.querySelector('#district');
+    let searchForm = document.querySelector('form');
+    let hospitalContainerElement = document.querySelector('#hospital-container');
+    let hospitalListElement = document.querySelector('#hospital-list');
+    let submitButton = document.querySelector('input[type="submit"]');
+
+    const submitForm = () => {
+        submitButton.click();
+    }
+    
+    const loadProvinces = () => {
+        return fetch("https://api.pedulilindungi.id/users/v3/provinces", {
+            "headers": {
+                "authorization": "Basic dGVsa29tOmRhMWMyNWQ4LTM3YzgtNDFiMS1hZmUyLTQyZGQ0ODI1YmZlYQ==",
+            }
+        })
+        .then(response => response.json())
+        .then(res => {
+            res.data.forEach(province => {
+                provinceSelectElement.innerHTML += `<option value="${province.provinceId}">${province.provinceName}</option>`;
+            });
+        })
+    }
+
+    const loadCities = () => {
+        return fetch(`https://api.pedulilindungi.id/users/v3/cities?provinceId=${provinceSelectElement.value}`, {
+            "headers": {
+                "authorization": "Basic dGVsa29tOmRhMWMyNWQ4LTM3YzgtNDFiMS1hZmUyLTQyZGQ0ODI1YmZlYQ==",
+            }
+        })
+        .then(response => response.json())
+        .then(res => {
+            citySelectElement.innerHTML = `<option value="" disabled selected>Kota/Kabupaten</option>`;
+            res.data.forEach(city => {
+                citySelectElement.innerHTML += `<option value="${city.cityId}">${city.cityName}</option>`
+            })
+        })
+    }
+
+    const loadDistricts = () => {
+        return fetch(`https://api.pedulilindungi.id/users/v3/districts?cityId=${citySelectElement.value}`, {
+            "headers": {
+                "authorization": "Basic dGVsa29tOmRhMWMyNWQ4LTM3YzgtNDFiMS1hZmUyLTQyZGQ0ODI1YmZlYQ==",
+            }
+        })
+        .then(response => response.json())
+        .then(res => {
+            districtSelectElement.innerHTML = `<option value="" disabled selected>Kecamatan</option>`;
+            res.data.forEach(district => {
+                districtSelectElement.innerHTML += `<option value="${district.districtId}">${district.districtName}</option>`
+            })
+        })
+    }
+
+    provinceSelectElement.addEventListener('change', (event) => {
+        loadCities()
+        .then(loadDistricts)
+        .then(submitForm);
+    })
+
+    citySelectElement.addEventListener('change', (event) => {
+        loadDistricts()
+        .then(submitForm);
+    })
+
+    districtSelectElement.addEventListener('change', (event) => {
+        submitForm();
+    })
+
+    searchForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let {search, provinceId, cityId, districtId} = event.target;
+
+        fetch(`https://api.pedulilindungi.id/vaccine/v1/landingpage/hospital?provinceId=${provinceId.value}&cityId=${cityId.value}&search=${search.value}&districtId=${districtId.value}&size=99&page=1`, {
+            "headers": {
+                "authorization": "Basic VkFLU0lOOjVmYmNkNjc4NDE2ZjVkOTUzMzFlODJhNA==",
+            }
+        })
+        .then(response => response.json())
+        .then(res => {
+            hospitalListElement.innerHTML = '';
+
+            res.data.forEach(hospital => {
+                hospitalListElement.innerHTML += 
+                `<div class="py-6">
+                    <h5 class="font-semibold mb-2">${hospital.name}</h5>
+                    <p>${hospital.address}</p>
+                </div>`;
+            })
+
+            hospitalContainerElement.classList.remove('hidden');
+            hospitalContainerElement.classList.add('flex');
+        })
+    })
+
+    loadProvinces();
+    
+</script>
+@endpush
