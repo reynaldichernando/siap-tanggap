fef05b045911fe5e2755e23598e52283bc223130
Reynaldi Chernando <reynaldichernando@gmail.com>
2021-08-05 16:22:43 +0700

    Add human readable time to news

--- app/Http/Controllers/Api/V1/NewsController.php
+++ app/Http/Controllers/Api/V1/NewsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -10,8 +11,12 @@ class NewsController extends Controller
 {
     public function index() {
         $API_KEY = '4814ef7797d2473f96671de33e8bec1a';
-        $response = Http::get('https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey='.$API_KEY);
+        $response = Http::get('https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey='.$API_KEY)->object();
+        
+        foreach ($response->articles as $article) {
+            $article->human_readable_time = Carbon::parse($article->publishedAt)->diffForHumans();
+        }
 
-        return $response->json();
+        return json_encode($response);
     }
 }
