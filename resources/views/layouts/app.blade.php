60b81a1df2b2bb3c329f666563da51971d75845d
Reynaldi Chernando <reynaldichernando@gmail.com>
2021-08-09 12:39:40 +0700

    Update username display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,7 +114,7 @@
                             href="{{ route('register') }}">{{ __('Register') }}</a>
                         @endif
                         @else
-                        <a href="{{ route('profile') }}" class="md:p-4 py-3 px-0 hover:opacity-75 duration-200">
+                        <a href="{{ route('profile') }}" class="md:p-4 py-3 px-0 block hover:opacity-75 duration-200">
                             {{ explode(' ',trim(Auth::user()->name))[0] }}
                         </a>
 
